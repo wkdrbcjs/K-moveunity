445b0b0d4a7f4c28a983e7b7bd9bcba997c88a32
허민구 <huccce@gmail.com>
2022-06-27 12:16:49 +0900

    캐릭터 이동
    - 캐릭터가 앞뒤로 움직일 수 있도록 함
    - 캐릭터가 스피드 값에 따라서 속도가 달라지도록 함
    - 캐릭터가 deltaTime으로 프레임 값에 상관 없이 일정한 속도로 움직이게 함
    - 캐릭터가 입력된 값에 따라서 회전하도록 함

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+
     // Start is called before the first frame update
     void Start()
     { 
@@ -14,7 +16,11 @@ public class Player : MonoBehaviour
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
-        Vector3 vector = new Vector3(h,0,0);
+        float v = Input.GetAxis("Vertical");
+        h = h * moveSpeed * Time.deltaTime;
+        v = v * moveSpeed * Time.deltaTime;
+        Vector3 vector = new Vector3(h,0,v);
         this.transform.position += vector;
+        transform.forward = vector;
     }
 }
