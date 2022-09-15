9884ed8d689ed14ce2b5e1fc8030462a14c2bd14
SeanM <>
2022-09-15 19:19:27 +0900

    좀비 애니메이션 추가
    이동, 공격

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,12 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     private State enemyState = State.Idle;
+    private Animator anim;
+
+    public GameObject target;
+
+    [SerializeField]
+    private float attackRange = 3f;
 
     public enum State
     {
@@ -15,18 +21,35 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Chase(target);
     }
 
     private void Chase(GameObject playerObj)
     {
-        GetComponent<NavMeshAgent>().destination = playerObj.transform.position;
+        float dis = Vector3.Distance(playerObj.transform.position, this.transform.position);
+
+        if (dis < attackRange)
+        {
+            enemyState = State.Attack;
+            GetComponent<NavMeshAgent>().enabled = false;
+            GetComponent<Animator>().SetBool("isWalk", false);
+            GetComponent<Animator>().SetBool("isAttack", true);
+        }
+        else 
+        {
+            GetComponent<Animator>().SetBool("isWalk", true);
+            enemyState = State.Chase;
+            GetComponent<NavMeshAgent>().enabled = true;
+            GetComponent<NavMeshAgent>().destination = playerObj.transform.position;
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
